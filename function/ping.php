382286c6ddc6f5cd9595e58a46fc4cc8adee91b7
Maxime Tournier <mxm.tournier@gmail.com>
2024-01-08 14:43:15 +0100

    update

--- function/ping.php
+++ function/ping.php
@@ -1,20 +1,11 @@
 <?php
 
-//function pingHost($server) {
-//
-//    $host = $server['url'];
-//
-//    $output = shell_exec("ping " . escapeshellarg($host));
-//
-//    echo "<pre>$output</pre>";
-//}
-
 function pingHost($server) {
 
     $name = $server['name'];
     $host = $server['url'];
 
-    if ($name == "vps202" || $host == "vps203.tyrolium.fr" || $name == "serv301" || $name == "int802"){
+    if ($name == "vps202" || $host == "vps203.tyrolium.fr" || $name == "serv301" || $name == "int802" || $name == "vps212" || $name == "vps215" || $name == "vps216" || $name == "vps217"){
 
         return "offline";
 
